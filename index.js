ac1305671116bdc308fd114ba9a0193bb90f79f2
oscmedgon <oscmedgon@gmail.com>
2018-01-01 00:51:08 +0100

    Adding more reactions to the bot

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const frasesChulas = [
+  'Yo..... soy..... tu Padri',
+  'Pero esto quien lo paga',
+  'Que no me robes mis frases',
+  'Hooola maldiiiito!!!',
+  'GÑAAAAA!!!!(Periko saludando)',
+  'Matar',
+  'EEEEEL PULPOOO!!!!',
+  'AAAASIENDA DEL PATRÓOOON!!!!',
+  'Feliz navidad!!!',
+  'A farmear plásticos'
+]
+
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
@@ -11,12 +24,25 @@ client.on('ready', () => {
   client.user.setGame('!hod para ser saludado')
 })
 client.on('message', message => {
-  console.log(message)
+  // Don't forget to log the message!
   const ignoreCaps = message.content.toLowerCase()
-  if (ignoreCaps.search('!hod') !== -1) {
-    message.react(':wink:')
-    message.reply('es la caña.')
+  if (ignoreCaps.search('hod') !== -1) {
+    message.react('👋')
+    salute(message)
   }
 })
+
+const salute = message => {
+  message.reply(fraseRandom())
+}
+
+const fraseRandom = () => frasesChulas[Math.floor(Math.random() * frasesChulas.length)]
+// client.on('message', message => {
+//   const ignoreCaps = message.content.toLowerCase()
+//   if (ignoreCaps.search('!hod') !== -1) {
+//     message.react('👋')
+//     message.reply('es la caña.')
+//   }
+// })
 
 client.login(token)
