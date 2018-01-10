369174c7bca111fb51b5743cdbdb6cec5087fa55
oscmedgon <oscmedgon@gmail.com>
2018-01-10 13:07:54 +0100

    Updated link

--- handlers/link.js
+++ handlers/link.js
@@ -1,17 +1,15 @@
 const axios = require('axios')
 
 // /api/discord/sign
-const link = message => {
+const link = async message => {
   message.react('👋')
   const params = `id=${message.author.id}&username=${message.author.username}`
   const user = {
     username: message.author.username,
     id: message.author.id
   }
-  axios.post('http://localhost:8080/api/discord/sign?', user)
-  .then(response => {
-    message.reply(`http://localhost:8080/api/discord/${response.data.token}`)
-  })
+  const {data} = await axios.post('http://localhost:8080/api/discord/sign?', user)
+  message.reply(`http://localhost:8080/api/discord/${data.token}`)
 }
 
 module.exports = link
