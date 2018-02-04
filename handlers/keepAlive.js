01d30d9845270862bad822402fa5e59916cc3782
oscmedgon <oscmedgon@gmail.com>
2018-02-04 12:32:01 +0100

    Sintax

--- handlers/keepAlive.js
+++ handlers/keepAlive.js
@@ -2,7 +2,7 @@
 const timer = (client) => {
   setInterval(() => {
     client.channels.get('409660944751525888').sendMessage(`Sigo vivo`)
-  }, 1740000 )
+  }, 1740000)
 }
 // 1740000 29 minutes
 const keepAlive = client => {
